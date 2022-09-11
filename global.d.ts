71798b7324e7a20517ac908fac3e120fb4240cdd
Amnesthesia <flowsnake@icloud.com>
2022-09-11 18:37:06 +1000

    fix: Typechecking (#92)

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,13 @@
 declare namespace global {
   function __reanimatedWorkletInit(): void;
 }
+
+declare module "react-native-skeleton-content/src" {
+  import { ISkeletonContentProps } from 'react-native-skeleton-content/src/Constants';
+  const SkeletonContent: React.ComponentType<ISkeletonContentProps>;
+  export default SkeletonContent;
+}
+
+declare module "react-native-redash/lib/module/v1" {
+  export * from 'react-native-redash/lib/typescript/v1';
+}
