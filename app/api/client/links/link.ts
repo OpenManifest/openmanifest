6b72111b258554f266e19380665b9965b8057fd5
Victor Rudolfsson <>
2023-01-15 22:20:59 +1000

    fix: Transpilation error for ActionCableLink

--- app/api/client/links/link.ts
+++ app/api/client/links/link.ts
@@ -2,7 +2,7 @@ import { ApolloLink } from '@apollo/client';
 import React from 'react';
 import { useAuthenticationLink } from './authentication';
 import { useErrorLink, defaultErrorLink } from './errors';
-import { httpWithWsLink, httpLink } from './http';
+import { createHttpLink, httpLink } from './http';
 import { useAppSignalLink } from './appSignal';
 
 export const defaultLink = ApolloLink.from([defaultErrorLink, httpLink]);
@@ -12,7 +12,7 @@ export function useLink() {
   const appSignalLink = useAppSignalLink();
 
   const links = React.useMemo(
-    () => [appSignalLink, errorLink, authLink, httpWithWsLink].filter(Boolean) as ApolloLink[],
+    () => [appSignalLink, errorLink, authLink, createHttpLink()].filter(Boolean) as ApolloLink[],
     [appSignalLink, authLink, errorLink]
   );
   return React.useMemo(() => ApolloLink.from(links), [links]);
--- app/api/client/links/websockets.ts
+++ app/api/client/links/websockets.ts
@@ -3,23 +3,26 @@ import ActionCableLink from 'graphql-ruby-client/subscriptions/ActionCableLink';
 import URI from 'urijs';
 import { getServerUrl } from '../utils/getServerUrl';
 
-const cable = createConsumer(
-  [
-    new URI(getServerUrl()).scheme() === 'https' ? 'wss://' : 'ws://',
-    new URI(getServerUrl()).host(),
-    '/subscriptions',
-  ].join('')
-);
 export const hasSubscriptionOperation = ({ query: { definitions } }) => {
   return definitions.some(
     ({ kind, operation }) => kind === 'OperationDefinition' && operation === 'subscription'
   );
 };
 
-export const websocketLink = new ActionCableLink({
-  cable,
-  connectionParams: (a) => {
-    const { authHeaders } = a.getContext();
-    return authHeaders;
-  },
-});
+export function createWebsocketsLink() {
+  const cable = createConsumer(
+    [
+      new URI(getServerUrl()).scheme() === 'https' ? 'wss://' : 'ws://',
+      new URI(getServerUrl()).host(),
+      '/subscriptions',
+    ].join('')
+  );
+
+  return new ActionCableLink({
+    cable,
+    connectionParams: (a) => {
+      const { authHeaders } = a.getContext();
+      return authHeaders;
+    },
+  });
+}
